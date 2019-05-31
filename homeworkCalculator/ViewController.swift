fc3a7b46df2e1501784b2cb05d1655f9eeb80750
榎本旭晃 <kingdomhearts.sora319@gmail.com>
2019-05-31 14:39:27 +0800

    電卓作成

--- homeworkCalculator/ViewController.swift
+++ homeworkCalculator/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+     @IBOutlet weak var xLabel: UILabel!
+     @IBOutlet weak var yLabel: UILabel!
+     @IBOutlet weak var resultLabel: UILabel!
+     @IBOutlet weak var signLabel: UILabel!
+     
      override func viewDidLoad() {
           super.viewDidLoad()
           // Do any additional setup after loading the view.
      }
 
 
+     @IBAction func num1Btn(_ sender: Any) {
+     }
+     @IBAction func num2Btn(_ sender: Any) {
+     }
+     @IBAction func num3Btn(_ sender: Any) {
+     }
+     @IBAction func num4Btn(_ sender: Any) {
+     }
+     @IBAction func num5Btn(_ sender: Any) {
+     }
+     @IBAction func num6Btn(_ sender: Any) {
+     }
+     @IBAction func num7Btn(_ sender: Any) {
+     }
+     @IBAction func num8Btn(_ sender: Any) {
+     }
+     @IBAction func num9Btn(_ sender: Any) {
+     }
+     @IBAction func num0Btn(_ sender: Any) {
+     }
+     @IBAction func addBtn(_ sender: Any) {
+     }
+     @IBAction func subBtn(_ sender: Any) {
+     }
+     @IBAction func mulBtn(_ sender: Any) {
+     }
+     @IBAction func divBtn(_ sender: Any) {
+     }
+     @IBAction func calBtn(_ sender: Any) {
+     }
+     @IBAction func clearBtn(_ sender: Any) {
+     }
+     
+     
 }
 
