779d302f45d1c5a35039222121a7fb6db2e89f86
榎本旭晃 <kingdomhearts.sora319@gmail.com>
2019-06-02 12:15:36 +0800

    機能変更
    電卓構成のメモを作成
    各数字と符号を入力する処理をそれぞれまとめる

--- homeworkCalculator/ViewController.swift
+++ homeworkCalculator/ViewController.swift
@@ -18,8 +18,8 @@ class ViewController: UIViewController {
      @IBOutlet weak var signLabel: UILabel!
     
 //    値の初期値
-    var x = 0
-    var y = 0
+    var x = ""
+    var y = ""
     
      override func viewDidLoad() {
           super.viewDidLoad()
@@ -35,40 +35,19 @@ class ViewController: UIViewController {
             signLabel.text = ""
         }
     }
+    // 数字を入力する処理
+    @IBAction func inputNumerBtn(_ sender: UIButton) {
+//        xLabel.text =  // クリックしたボタンの数字め
+//        yLabel.text = クリックしたボタンの数字
+    }
+    
+    // 符号を選択する処理
+    @IBAction func choseSignBtn(_ sender: UIButton) {
+//        signLabel = クリックしたボタンの符号
+    }
+    
 }
 
-// 数字を入力する処理
-extension ViewController{
-     @IBAction func num1Btn(_ sender: Any) {
-     }
-     @IBAction func num2Btn(_ sender: Any) {
-     }
-     @IBAction func num3Btn(_ sender: Any) {
-     }
-     @IBAction func num4Btn(_ sender: Any) {
-     }
-     @IBAction func num5Btn(_ sender: Any) {
-     }
-     @IBAction func num6Btn(_ sender: Any) {
-     }
-     @IBAction func num7Btn(_ sender: Any) {
-     }
-     @IBAction func num8Btn(_ sender: Any) {
-     }
-     @IBAction func num9Btn(_ sender: Any) {
-     }
-     @IBAction func num0Btn(_ sender: Any) {
-     }
-}
 
-// 符号を選択する処理
-extension ViewController {
-     @IBAction func addBtn(_ sender: Any) {
-     }
-     @IBAction func subBtn(_ sender: Any) {
-     }
-     @IBAction func mulBtn(_ sender: Any) {
-     }
-     @IBAction func divBtn(_ sender: Any) {
-     }
-}
+
+
